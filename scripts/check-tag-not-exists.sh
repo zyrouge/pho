58ca3b8f9c5181cb08b3e972df455a4714b6964e
zyrouge <zyrouge@hotmail.com>
2023-10-05 19:40:29 +0530

    ci: fix bash if

--- scripts/check-tag-not-exists.sh
+++ scripts/check-tag-not-exists.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-git ls-remote --exit-code --tags origin $1
+tag=$1
+git ls-remote --exit-code --tags origin $tag
 status=$?
-if [[ status -eq 0 ]] then
+if [ $status == 0 ]; then
+    echo "Tag $tag is not available"
     exit 1
 fi
 
+echo "Tag $tag is available"
 exit 0
