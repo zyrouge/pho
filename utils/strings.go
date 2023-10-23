3f7f54f3f95c7b8d082923ad350226e46d766b4c
zyrouge <zyrouge@hotmail.com>
2023-10-23 16:07:52 +0530

    chore: lint

--- utils/strings.go
+++ utils/strings.go
@@ -1,10 +1,8 @@
 package utils
 
 import (
-	"encoding/json"
 	"regexp"
 	"strings"
-	"unicode"
 )
 
 var cleanTextInvalidRegex = regexp.MustCompile(`[^A-Za-z0-9-]+`)
@@ -16,42 +14,10 @@ func ReplaceIllegalChars(text string) string {
 	return text
 }
 
-func CleanText(text string) string {
-	text = ReplaceIllegalChars(text)
-	return text
-}
-
 func CleanId(text string) string {
 	text = ReplaceIllegalChars(text)
 	text = strings.ToLower(text)
 	return text
-}
-
-func CleanName(text string) string {
-	text = ReplaceIllegalChars(text)
-	text = TitleCase(text)
-	return text
-}
-
-var titleCaseReplaceRegex = regexp.MustCompile(`\w{2,}`)
-
-func TitleCase(text string) string {
-	return titleCaseReplaceRegex.ReplaceAllStringFunc(
-		text,
-		func(s string) string {
-			runes := []rune(s)
-			runes[0] = unicode.ToUpper(runes[0])
-			return string(runes)
-		},
-	)
-}
-
-func PrettyJSONString(data any) (string, error) {
-	bytes, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
-		return "", err
-	}
-	return string(bytes), nil
 }
 
 func QuotedWhenSpace(text string) string {
